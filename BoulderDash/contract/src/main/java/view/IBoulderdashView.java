1e624e6aa942436c50d67bd0a46bdbceab4abdd9
julien-juncker <julien.juncker@viacesi.fr>
2017-07-06 15:18:13 +0200

    ajout des interfaces vue

---
+++ BoulderDash/contract/src/main/java/view/IBoulderdashView.java
@@ -0,0 +1,24 @@
+package view;
+
+/**
+ * <h1>The Interface IInsaneVehiclesView.</h1>
+ *
+ * @author Jade
+ * @version 0.1
+ */
+public interface IBoulderdashView {
+
+    /**
+     * Display message.
+     *
+     * @param message
+     *            the message
+     */
+    void displayMessage(String message);
+
+    /**
+     * Follow myVehicle.
+     */
+    void followMyVehicle();
+
+}
