3662987c5ddec9da1df7bfcc014971472b567ae3
julien-juncker <julien.juncker@viacesi.fr>
2017-07-07 11:43:42 +0200

    import main

--- BoulderDash/main/src/main/java/main/Main.java
+++ BoulderDash/main/src/main/java/main/Main.java
@@ -1,33 +1,43 @@
 package main;
 
-import java.sql.SQLException;
+import java.io.IOException;
 
-import controller.ControllerFacade;
-import model.ModelFacade;
-import view.ViewFacade;
+import controller.IBoulderdashController;
+import controller.BoulderdashController;
+import model.IBoulderdashModel;
+import model.BoulderdashModel;
+import view.BoulderdashView;
 
 /**
- * <h1>The Class Main.</h1>
+ * <h1>The InsaneVehicles Class.</h1>
  *
- * @author Jean-Aymeric DIET jadiet@cesi.fr
- * @version 1.0
+ * @author Jade
+ * @version 0.2
  */
 public abstract class Main {
+
+    /** The Constant startX. */
+    private static final int startX = 5;
+
+    /** The Constant startY. */
+    private static final int startY = 0;
 
     /**
      * The main method.
      *
      * @param args
      *            the arguments
+     * @throws IOException
+     *             Signals that an I/O exception has occurred.
+     * @throws InterruptedException
+     *             the interrupted exception
      */
-    public static void main(final String[] args) {
-        final ControllerFacade controller = new ControllerFacade(new ViewFacade(), new ModelFacade());
+    public static void main(final String[] args) throws IOException, InterruptedException {
+        final IBoulderdashModel model = new BoulderdashModel("road.txt", startX, startY);
+        final BoulderdashView view = new BoulderdashView(model.getGround(), model.getMyVehicle());
+        final IBoulderdashController controller = new BoulderdashController(view, model);
+        view.setOrderPerformer(controller.getOrderPeformer());
 
-        try {
-            controller.start();
-        } catch (final SQLException exception) {
-            exception.printStackTrace();
-        }
+        controller.play();
     }
-
 }
